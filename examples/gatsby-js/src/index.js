417529696a281922b70fd68fcea2dd497c089bdd
James Edmonds <james@deckchairlabs.com>
2020-11-22 11:30:07 +1030

    chore: cleanup

--- examples/gatsby-js/src/index.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export const wrapRootElement = ({ element }) => <div>{element}</div>;
--- src/var-names.ts
+++
@@ -1,3 +0,0 @@
-const toVarName = (key: string) => `--theme-ui-colors-${key}`
-export const toVarValue = (key: string, value?: string | number) =>
-  `var(${toVarName(key)}${value ? `, ${value}` : ''})`
