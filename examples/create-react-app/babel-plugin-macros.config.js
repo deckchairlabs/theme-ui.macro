9dc09f1ec473004d9ada29f58448b48b2825b8c8
James Edmonds <james@deckchairlabs.com>
2020-11-26 17:56:30 +1030

    docs: added stylesheet plugin to example

--- examples/create-react-app/babel-plugin-macros.config.js
+++ examples/create-react-app/babel-plugin-macros.config.js
@@ -1,11 +1,19 @@
 const path = require('path')
 const customProperties = require('theme-ui.macro/plugins/customProperties').default
+const generateStylesheet = require('theme-ui.macro/plugins/generateStylesheet').default
 const typescriptDeclarations = require('theme-ui.macro/plugins/typescriptDeclarations').default
 
 module.exports = {
     themeUI: {
-        plugins: [customProperties({ prefix: 'theme-ui' }), typescriptDeclarations({
-            output: path.join(__dirname, 'src/theme.d.ts')
-        })]
+        plugins: [
+            customProperties({ prefix: 'theme-ui' }),
+            generateStylesheet({
+                selectors: { buttons: '.button' },
+                output: path.join(__dirname, 'src/theme.css'),
+            }),
+            typescriptDeclarations({
+                output: path.join(__dirname, 'src/theme.d.ts')
+            })
+        ]
     },
 }
