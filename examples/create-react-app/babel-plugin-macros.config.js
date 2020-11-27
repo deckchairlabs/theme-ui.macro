039a4014af63481db09afd54827b07b83626c7bc
James Edmonds <james@deckchairlabs.com>
2020-11-27 12:05:37 +1030

    docs: updated example to use latest experimental release

--- examples/create-react-app/babel-plugin-macros.config.js
+++ examples/create-react-app/babel-plugin-macros.config.js
@@ -1,19 +1,14 @@
 const path = require('path')
-const customProperties = require('theme-ui.macro/plugins/customProperties').default
-const generateStylesheet = require('theme-ui.macro/plugins/generateStylesheet').default
-const typescriptDeclarations = require('theme-ui.macro/plugins/typescriptDeclarations').default
 
 module.exports = {
     themeUI: {
-        plugins: [
-            customProperties({ prefix: 'theme-ui' }),
-            generateStylesheet({
-                selectors: { buttons: '.button' },
-                output: path.join(__dirname, 'src/theme.css'),
-            }),
-            typescriptDeclarations({
-                output: path.join(__dirname, 'src/theme.d.ts')
-            })
-        ]
+        customProperties: { prefix: 'theme-ui' },
+        generateStylesheet: {
+            selectors: { buttons: '.button' },
+            output: path.join(__dirname, 'src/theme.css'),
+        },
+        generateTSDeclaration: {
+            output: path.join(__dirname, 'src/theme.d.ts')
+        }
     },
 }
